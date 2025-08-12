964177dfc8a0d3d7d201036969091d179bad8d86
ahaanav10 <ahaanavar@gmail.com>
2025-08-12 10:24:05 -0700

    Add files via upload
    finished initials

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
+    World.setDelay(4);
 
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
-    Robot r = new Robot(2,8,East,20);
+    Robot r = new Robot(2,8,East,30);
 		
     // examples of commands you can invoke on a Robot
     r.move();
@@ -19,23 +20,6 @@ public class Main implements Directions{
     r.move();
     r.move();
     r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
     r.move();
     r.putBeeper();
     r.turnLeft();
@@ -51,6 +35,24 @@ public class Main implements Directions{
     r.putBeeper();
     r.turnLeft();
     r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
     r.move();
     r.move();
     r.turnLeft();
@@ -72,7 +74,27 @@ public class Main implements Directions{
     r.turnLeft();
     r.turnLeft();
     r.move();
+    r.turnLeft();
+    r.move();
     r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.turnLeft();
+    r.move();    
+    r.putBeeper();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+
 
     
 
